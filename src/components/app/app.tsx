709cd2fca99f12f9e82a25724478cb7058c35eb2
Nade <s.nadjufka@gmail.com>
2022-04-08 16:56:31 +0300

    ErrorBoundary (#7)
    * ErrorBoundary
    
    * Error boundary fix

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -7,21 +7,25 @@ import LeaderboardPage from '../pages/leaderboard-page';
 import ForumPage from '../pages/forum-page';
 import MainPage from '../pages/main-page';
 import NotFoundPage from '../pages/not-found-page';
+import ErrorBoundaryComponent from '../common/error-boundary';
+import ErrorFallbackComponent from '../common/error-fallback';
 
 function App() {
   return (
     <div className="app">
-      <Router>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/sign-in" element={<LoginPage />} />
-          <Route path="/sign-up" element={<RegisterPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-          <Route path="/leaderboard" element={<LeaderboardPage />} />
-          <Route path="/forum" element={<ForumPage />} />
-          <Route path="/*" element={<NotFoundPage />} />
-        </Routes>
-      </Router>
+      <ErrorBoundaryComponent FallbackComponent={ErrorFallbackComponent}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/sign-in" element={<LoginPage />} />
+            <Route path="/sign-up" element={<RegisterPage />} />
+            <Route path="/settings" element={<SettingsPage />} />
+            <Route path="/leaderboard" element={<LeaderboardPage />} />
+            <Route path="/forum" element={<ForumPage />} />
+            <Route path="/*" element={<NotFoundPage />} />
+          </Routes>
+        </Router>
+      </ErrorBoundaryComponent>
     </div>
   );
 }
---
+++ src/components/common/error-fallback/error-fallback.component.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from 'react';
+import {FallbackProps} from '../error-boundary';
+
+import './error-fallback.scss';
+
+const ErrorFallbackComponent: FC<FallbackProps> = (props: FallbackProps) => {
+  return (
+    <div className="error-text-container">
+      {/*TODO вынести в enum*/}
+      <h2>{'Что-то пошло не так ^_^'}</h2>
+      <p>{'Возникла непредвиденная ошибка. Пожалуйста, попробуйте позже.'}</p>
+    </div>
+  );
+};
+
+export default ErrorFallbackComponent;
