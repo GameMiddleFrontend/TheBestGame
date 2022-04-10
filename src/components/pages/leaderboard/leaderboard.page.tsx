59fd3713d587d444279f701780532b09829812b3
Nade <s.nadjufka@gmail.com>
2022-04-10 14:48:08 +0300

    Таблица лидеров

--- src/components/pages/leaderboard/leaderboard.page.tsx
+++ src/components/pages/leaderboard/leaderboard.page.tsx
@@ -1,20 +1,37 @@
 import React from 'react';
+import {LeaderboardUserType} from './leaderboard.types';
 
-function LeaderboardPage() {
-  const leaderList = [{userName: 'userName', points: 123123123}];
+const LeaderboardPage = () => {
+  const leaderList: LeaderboardUserType[] = [
+    {userName: 'userName', points: 123123123},
+    {userName: 'userName2', points: 123123123},
+    {userName: 'userName3', points: 123123123},
+  ];
 
-  const LeaderboardRow = (props: {position: number; avatar: string; userName: string; points: number}) => {
-    return (
-      <div>
-        <div>{props.position}</div>
-        <div>{props.avatar}</div>
-        <div>{props.userName}</div>
-        <div>{props.points}</div>
-      </div>
-    );
-  };
+  return (
+    <div className="page">
+      <h2>Таблица лидеров</h2>
+      <table>
+        <tr>
+          <th>Место</th>
+          <th colSpan={2}>Игрок</th>
+          <th>Счет</th>
+        </tr>
+        {leaderList.map((item, index) => {
+          const position = index + 1;
 
-  return <div className="page">LeaderboardPage</div>;
-}
+          return (
+            <tr key={position}>
+              <td>{position}</td>
+              <td>{item.avatar}</td> {/*TODO добавится компонент аватара, из другой ветки*/}
+              <td>{item.userName}</td>
+              <td>{item.points}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
+};
 
 export default LeaderboardPage;
