a31f04f01875901e073b8058489ebbc5189e02ca
Nade <s.nadjufka@gmail.com>
2022-04-25 20:03:52 +0300

    Tests Snapshot

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less|scss)$": "<rootDir>/__mocks__/styleMock.js"
+  },
+};
---
+++ src/components/common/button/button.spec.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Button from './button';
+import cancelArrowImg from '../../../styles/images/cancel-arrow.svg';
+
+describe('Button', () => {
+  it('ButtonText Snapshot', () => {
+    const tree = renderer.create(<Button className={'button button-text'}>TextButton</Button>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it('ButtonIconOnly Snapshot', () => {
+    const tree = renderer.create(<Button className={'button button-icon-only'} icon={cancelArrowImg} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
