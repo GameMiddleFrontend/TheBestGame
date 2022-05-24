c96043e4987e34d014127f4f1375f4b2bb223b52
Nade <s.nadjufka@gmail.com>
2022-05-24 11:03:19 +0300

    Чистка мусора

--- src/utils/helpers/isArray.ts
+++
@@ -1,5 +0,0 @@
-function isArray(value: unknown): value is [] {
-  return Array.isArray(value);
-}
-
-export default isArray;
--- src/utils/helpers/wait.ts
+++
@@ -1,6 +0,0 @@
-const syncWait = (ms: number) => {
-  const end = Date.now() + ms;
-  while (Date.now() < end) continue;
-};
-
-export default syncWait;
