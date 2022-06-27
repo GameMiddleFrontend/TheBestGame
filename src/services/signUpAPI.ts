c45359abb18ea88ab9ab2d0a3b0302cffed89a4f
ASviridov <amsviridov@simple.ru>
2022-06-27 14:55:35 +0300

    register api fix

--- src/services/signUpAPI.ts
+++ src/services/signUpAPI.ts
@@ -1,28 +1,10 @@
 import {FormikValues} from 'formik';
-import APIUtils from './service.utils';
-
-type SignUpError = {
-  reason: string;
-};
+import ServiceUtils from './service.utils';
 
 class SignUpAPI {
   static async signUp(body: FormikValues): Promise<string> {
-    const response = await APIUtils.getFetch(
-      {
-        method: 'POST',
-        headers: APIUtils.simpleJsonHeader,
-        body: JSON.stringify(body),
-        keepalive: true,
-        credentials: 'include',
-      },
-      '/auth/signup',
-    );
-    let result: SignUpError | string;
-    if (!response.ok) {
-      result = await response.json();
-      throw new Error((result as SignUpError).reason);
-    }
-    return response.text();
+    const response = await ServiceUtils.post('/auth/signup', body);
+    return response;
   }
 }
 
